fae4f0805ab0129dd9717ad925a6ffe8300277cd
alfredo.landaeta <alfredo.landaeta@ALFREDOLANDAET.pragma.com.co>
2020-03-10 20:42:08 -0500

    project base init done

---
+++ src/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppProvider from './containers/AppProvider/AppProvider';
+
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+
+describe('Application root', () => {
+    it('should render without crashing', () => {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'root');
+        document.body.appendChild(div);
+        // eslint-disable-next-line global-require
+        require('./index');
+        expect(ReactDOM.render).toHaveBeenCalledWith(<AppProvider />, div);
+    });
+});
