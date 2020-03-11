22fa99486ac5b1a09d44c5b4d75fff3b15e3e109
Luis Alfredo Landaeta Espinoza <alfredo.landaeta@PRAGMA.COM.CO>
2020-03-11 18:39:50 -0500

    Updates

---
+++ precache-manifest.bd433eeb784ac9e40a476ffa6a561f4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65eef3de187fab28fa72637a8a266646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f0f3edc6f009a8a438",
+    "url": "/static/js/2.8f1ec97c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.8f1ec97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4b7b1a2c7596b19c85",
+    "url": "/static/js/main.a85eefb8.chunk.js"
+  },
+  {
+    "revision": "b5b9e7a20298817e8843",
+    "url": "/static/js/runtime-main.2168a4bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/peturpet/precache-manifest.8370ae794d1eb7fc60b0e3c14bbc96e2.js"
+  "/precache-manifest.bd433eeb784ac9e40a476ffa6a561f4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/peturpet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
